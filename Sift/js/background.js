8ab0a961911e4d5051c10eef51c1a8eef92c986d
Jing Yi Wang <jingyi@Moonstone.lan>
2019-08-10 20:06:33 -0400

    small updates to ui

--- Sift/js/background.js
+++ Sift/js/background.js
@@ -1,7 +1,6 @@
 // background.js is always running while the extnesion is on
 // listens to events like key presses or navigating to different pages
-/*
+
 chrome.runtime.onInstalled.addListener(function() {
     alert("background.js working");
 });
-*/
